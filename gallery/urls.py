a8a390e474d0d47e918677f02b8ea943b679bd0f
verasmarianna <mariannamaldiveras@gmail.com>
2022-10-16 18:30:02 -0300

    subindo desafio 2 (com erro de formatacao da pag)

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from gallery.views import index, imagem
+from gallery.views import index, imagem, imagem2, imagem3, imagem4, imagem5, imagem6
 
 urlpatterns = [
     path ('', index),
     path ('imagem/', imagem, name='imagem'),
+    path ('imagem2/', imagem2, name='imagem2'),
+    path ('imagem3/', imagem3, name='imagem3'),
+    path ('imagem4/', imagem4, name='imagem4'),
+    path ('imagem5/', imagem5, name='imagem5'),
+    path ('imagem6/', imagem6, name='imagem6'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,4 @@
+from pickletools import read_int4
 from django.shortcuts import render
 
 def index(request):
@@ -6,4 +7,18 @@ def index(request):
 def imagem (request):
     return render (request, 'gallery/imagem.html')
 
-# Create your views here.
+def imagem2 (request):
+    return render (request, 'gallery/imagem2.html')
+
+def imagem3 (request):
+    return render (request, 'gallery/imagem3.html')
+
+def imagem4 (request):
+    return render (request, 'gallery/imagem4.html')
+
+def imagem5 (request):
+    return render (request, 'gallery/imagem5.html')
+
+def imagem6 (request):
+    return render (request, 'gallery/imagem6.html')
+
